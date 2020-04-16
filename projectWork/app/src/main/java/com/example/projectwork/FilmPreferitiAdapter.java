54646f4af97bb4ec304f61217b1100d0644b7c31
Emanuele <emanuele_volpato@outlook.it>
2020-04-16 10:21:07 +0200

    inizio implementazione lista film preferiti

---
+++ projectWork/app/src/main/java/com/example/projectwork/FilmPreferitiAdapter.java
@@ -0,0 +1,64 @@
+package com.example.projectwork;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+
+public class FilmPreferitiAdapter extends CursorAdapter {
+
+
+    public FilmPreferitiAdapter(Context context, Cursor c) {
+        super(context, c);
+    }
+
+    @Override
+    public View newView(final Context context, Cursor cursor, ViewGroup parent) {
+        LayoutInflater vInflater = LayoutInflater.from(context);
+        final View vView = vInflater.inflate(R.layout.cell_lista_film_preferiti, null);
+        final ImageView imageFilmPreferiti;
+
+        imageFilmPreferiti =vView.findViewById(R.id.imageViewFilmPreferiti);
+
+        imageFilmPreferiti.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                alert.setTitle("FILM GIA' AGGIUNTO AI PREFERITI!");
+                alert.setMessage("Togliere il film selezionato dai preferiti?");
+                alert.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "FILM TOLTO DAI PREFERITI!", Toast.LENGTH_LONG).show();
+                    }
+                });
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "FILM NON TOLTO DAI PREFERITI!", Toast.LENGTH_LONG).show();
+                    }
+                });
+                AlertDialog alert1 = alert.create();
+                alert1.show();
+                return true;
+            }
+
+        });
+
+        return vView;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+    }
+}
--- projectWork/app/src/main/java/com/example/projectwork/ListaFilmPreferiti.java
+++ projectWork/app/src/main/java/com/example/projectwork/ListaFilmPreferiti.java
@@ -1,12 +1,23 @@
 package com.example.projectwork;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.ListView;
 
-public class ListaFilmPreferiti extends AppCompatActivity {
+import com.example.projectwork.localDatabase.FilmProvider;
 
+public class ListaFilmPreferiti extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    ListView listViewFilmPreferiti;
+    FilmPreferitiAdapter mAdapter;
+    public static final int MY_LOADER_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +25,27 @@ public class ListaFilmPreferiti extends AppCompatActivity {
         setContentView(R.layout.activity_lista_film_preferiti);
         getSupportActionBar().setTitle("LISTA FILM PREFERITI");
 
+        listViewFilmPreferiti = findViewById(R.id.listFilmPreferiti);
+        mAdapter = new FilmPreferitiAdapter(this, null);
+        listViewFilmPreferiti.setAdapter(mAdapter);
+        getSupportLoaderManager().initLoader(MY_LOADER_ID, null, this);
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return new CursorLoader(this, FilmProvider.FILMS_URI, null, null, null, null);
+
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        mAdapter.changeCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        mAdapter.changeCursor(null);
     }
 }
