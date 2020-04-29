cf0e04d2a7d3813573327759dd1ba5a4dddead4d
Emanuele <emanuele_volpato@outlook.it>
2020-04-29 09:17:24 +0200

    fix scermata logo cambio tema

--- projectWork/app/src/main/java/com/example/projectwork/activity/MainActivity.java
+++ projectWork/app/src/main/java/com/example/projectwork/activity/MainActivity.java
@@ -69,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements IWebService {
 
     SharedPref sharedPref;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
